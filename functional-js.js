b58861e5d46d41feb81567341e67301fa1297f95
stardustrain <kthanterran@gmail.com>
2017-09-01 00:50:00 +0900

    Add _each function.

--- functional-js.js
+++ functional-js.js
@@ -1,19 +1,27 @@
-var _filter = function (array, condition) {
-  var returnArray = [];
+function _each(array, iteratee) {
+  for(var i = 1; i < array.length; i++) {
+    iteratee(array[i]);
+  }
+
+  return array;
+}
+
+function _filter(array, predicate) {
+  var newList = [];
   for (var i = 1; i < array.length; i++) {
-    if (condition(array[i])) {
-      returnArray.push(array[i]);
+    if (predicate(array[i])) {
+      newList.push(array[i]);
     }
   }
 
-  return returnArray;
+  return newList;
 }
 
-var _map = function (array, iteratee) {
-  var returnArray = [];
+function _map(array, mapper) {
+  var newList = [];
   for (var i = 1; i < array.length; i++) {
-    returnArray.push(iteratee(array[i]))
+    newList.push(mapper(array[i]))
   }
 
-  return returnArray;
+  return newList;
 }
